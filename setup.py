47bd4461249760ca7a50c45f0d6a557983d61225
Noah D. Brenowitz <nbren12@gmail.com>
2014-09-28 21:02:13 -0400

    packaging better

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from distutils.extension import Extension
 from Cython.Distutils import build_ext
 import numpy as np                           # <---- New line
 
-ext_modules = [Extension("gslrand",
-    sources = ["gslrand.pyx"],
+ext_modules = [Extension("gnl.gslrand",
+    sources = ["src/gnl/gslrand.pyx"],
     libraries = ['gsl', 'cblas']
     )]
 
@@ -13,6 +13,9 @@ setup(
     name = 'gnl',
     cmdclass = {'build_ext': build_ext},
     # include_dirs = [np.get_include()],         # <---- New line
-    ext_modules = ext_modules
+    ext_modules = ext_modules,
+    packages    = ['gnl'],
+    package_dir = {'': 'src'},
+
 )
 # -lmkl_intel_lp64 -lmkl_sequential -lmkl_core -lpthread -lm
