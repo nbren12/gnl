554dd2fc58a97051671036d912ada0041c5534eb
Noah D. Brenowitz <nbren12@gmail.com>
2014-11-08 18:17:37 -0500

    reverted stuff

--- gnl/filter/lorenz63.py
+++ gnl/filter/lorenz63.py
@@ -19,21 +19,6 @@ def observe(arr, i=Ellipsis, axis=0):
 
     return arr[s]
 
-class Lorenz63State(State):
-    """Wraps the loading and observing operators"""
-
-    def __init__(self, *args, obs='krza', **kwargs):
-        super(Lorenz63State, self).__init__(*args, **kwargs)
-
-        fullobs = 'krza'
-
-        self.obs_slice = [fullobs.index(ob) for ob in obs]
-        self.obs = obs
-
-
-    def observe(self, **kw):
-        return G(self._arr, **kw)
-
 # Right hand side of loren63 system
 def rhs(U, t, sigma=10, rho=28, b=8/3):
     x = U[0]
@@ -55,20 +40,22 @@ nt = 400
 nv = 3
 
 # Run exact model
+G = observe
 tout = np.arange(nt) * dt
 y_truth = odeint(rhs, [1.0, 1.0, 1.0], tout)
 
 
-G = observe
 Ro = np.eye(3) * 4**2
+
 obs = G(y_truth) + multivariate_normal(np.ones(nv), Ro, len(tout))
 
 # Initialize ensemble
 ne = 10
 ens = np.ones((nv, ne)) + multivariate_normal(np.ones(nv), Ro*.1, ne).T
 
+
 # Analysis
-analyzer = SequentialKFAnalysis(Lorenz63State, np.diag(Ro))
+analyzer = SequentialKFAnalysis(G, np.diag(Ro))
 
 # Filter driver object
 fd = FilterDriver(ens, evolve63, analyzer)
