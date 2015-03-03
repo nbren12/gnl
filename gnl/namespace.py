a02c8f4a1c24d9e717fb8372b202320e6f6d9e5e
Noah D. Brenowitz <nbren12@gmail.com>
2015-03-03 10:21:22 -0500

    added a pylab style namespace

---
+++ gnl/namespace.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+import xray
+import netCDF4 as nc
+
+from numpy.fft import fft, ifft, fftshift, fftfreq
+from numpy import percentile
+from numpy.random import rand, randn
+
+from scipy.integrate import quadrature
+from scipy.interpolate import interp1d
+
+import matplotlib.pyplot as plt
