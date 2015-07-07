7baa183068636406688244703dadff80a2afa7d5
Noah D. Brenowitz <nbren12@gmail.com>
2015-07-07 18:17:06 -0400

    added integrate function

--- gnl/xray.py
+++ gnl/xray.py
@@ -1,4 +1,5 @@
 import xray
+import numpy as np
 
 def xargs(z):
     """
@@ -11,3 +12,17 @@ def xargs(z):
     x = z.coords[dims[1]].values
 
     return x, y, z.values
+
+def integrate(x, axis='z'):
+    """
+    Integrate a dataframe along an axis using np.trapz
+    """
+    axisnum = list(x.dims).index(axis)
+    
+    dims = list(x.dims)
+    del dims[axisnum]
+    
+    coords = {key:x.coords[key] for key in dims}
+    
+    tot = np.trapz(x.values, x=x.coords[axis], axis=axisnum)
+    return xray.DataArray(tot, coords, dims)
