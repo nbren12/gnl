1494693b7f5587f10f85f823968e4d86aadc02b7
Noah D. Brenowitz <nbren12@gmail.com>
2015-07-19 17:30:03 -0400

    moved nbsubplots to plots.py

--- gnl/plots.py
+++ gnl/plots.py
@@ -6,6 +6,17 @@ article_style = {
     'axes.labelsize': 'small'
 }
 
+def nbsubplots(nrows=1, ncols=1, w=None, h=1.0, aspect=1.0, **kwargs):
+    """Make a set of axes with fixed aspect ratio"""
+    from matplotlib import pyplot as plt
+
+    if w is not None:
+        h = w * aspect
+    else:
+        w = h / aspect
+
+    return  plt.subplots(nrows,ncols, figsize=(w * ncols ,h * nrows), **kwargs)
+
 def figlabel(*args, fig=None, **kwargs):
     """Put label in figure coords"""
     if fig is None:
--- gnl/util.py
+++ gnl/util.py
@@ -17,23 +17,15 @@ from IPython.terminal.embed import InteractiveShellEmbed
 # where you want it to open.
 ipshell = InteractiveShellEmbed(banner1=banner, exit_msg=exit_msg)
 
+
+## Functional programming stuff
 def argkws(f):
     """Function decorator which maps f((args, kwargs)) to f(*args, **kwargs)"""
     @wraps(f)
     def fun(tup):
         args, kws = tup
-        return fun(*args, **kw)
+        return f(*args, **kw)
 
-def nbsubplots(nrows=1, ncols=1, w=None, h=1.0, aspect=1.0, **kwargs):
-    """Make a set of axes with fixed aspect ratio"""
-    from matplotlib import pyplot as plt
-
-    if w is not None:
-        h = w * aspect
-    else:
-        w = h / aspect
-
-    return  plt.subplots(nrows,ncols, figsize=(w * ncols ,h * nrows), **kwargs)
 
 def vdot(*arrs, l2r=True):
     """Variadic numpy dot function
