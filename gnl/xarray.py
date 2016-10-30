61054391e60c63204bd916a66cb52cb3996b96fb
Noah D. Brenowitz <nbren12@gmail.com>
2016-10-30 01:46:52 -0400

    add custom methods to xarray.DataArray

--- gnl/xarray.py
+++ gnl/xarray.py
@@ -1,4 +1,4 @@
-import xarray as xarray
+import xarray as xr
 import numpy as np
 from . import util
 
@@ -15,19 +15,20 @@ def xargs(z):
 
     return x, y, z.values
 
+
 def integrate(x, axis='z'):
     """
     Integrate a dataframe along an axis using np.trapz
     """
     axisnum = list(x.dims).index(axis)
-    
+
     dims = list(x.dims)
     del dims[axisnum]
-    
-    coords = {key:x.coords[key] for key in dims}
-    
+
+    coords = {key: x.coords[key] for key in dims}
+
     tot = np.trapz(x.values, x=x.coords[axis], axis=axisnum)
-    return xarray.DataArray(tot, coords, dims)
+    return xr.DataArray(tot, coords, dims)
 
 
 def phaseshift(u500, c=0):
@@ -35,11 +36,16 @@ def phaseshift(u500, c=0):
     phaseshift data into a travelling wave frame
     """
     # TODO: add arguments for x and time names
-    z  = util.phaseshift(u500.x.values, u500.time.values, u500.values, c=c, x_index=-1, time_index=0)
-    
+    z = util.phaseshift(u500.x.values,
+                        u500.time.values,
+                        u500.values,
+                        c=c,
+                        x_index=-1,
+                        time_index=0)
+
     out = u500.copy()
-    out.values= z
-    
+    out.values = z
+
     return out
 
 
@@ -52,15 +58,12 @@ def roll(z, **kwargs):
     roll(U, x=400)
     """
     from scipy import ndimage
-    sw = [kwargs[dim] if dim in kwargs else 0
-          for dim in z.dims]
-        
+    sw = [kwargs[dim] if dim in kwargs else 0 for dim in z.dims]
+
     zout = ndimage.shift(z, sw, mode='wrap')
-    
-    
-    
+
     out = z.copy()
-    out.values= zout
+    out.values = zout
     return out
 
 
@@ -74,3 +77,8 @@ def remove_repeats(data, dim='time'):
         inds.append(ival)
 
     return data[{dim: inds}]
+
+
+xr.DataArray.integrate = integrate
+xr.DataArray.roll = roll
+xr.DataArray.remove_repeats = remove_repeats
