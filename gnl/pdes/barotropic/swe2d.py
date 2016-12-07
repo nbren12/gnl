332207ff4d73c34e0363caf48b114ab9ac79a449
Noah D. Brenowitz <nbren12@gmail.com>
2016-12-07 15:18:25 -0500

    specify flux and source terms ahead of time

--- gnl/pdes/barotropic/swe2d.py
+++ gnl/pdes/barotropic/swe2d.py
@@ -13,16 +13,229 @@ from gnl.pdes.grid import ghosted_grid
 from gnl.pdes.timestepping import steps
 from gnl.pdes.tadmor.tadmor_2d import MultiFab
 from gnl.io import NetCDF4Writer
-from gnl.pdes.barotropic.swe import SWE2NonlinearSolver
+from gnl.pdes.barotropic.swe import *
+
+from gnl.pdes.barotropic.barotropic import BarotropicSolver
+from gnl.pdes.tadmor.tadmor import divergence
+
+from gnl.pdes.barotropic.swe import nemult
+
+class ixer(object):
+    def __init__(self, uc, inds):
+        self.uc = uc
+        self.inds = inds
+
+    def __getitem__(self, key):
+        return self.uc[self.inds.index(key)]
+
+    def __setitem__(self, key, val):
+        self.uc[self.inds.index(key)] = val
+
+class SWE2Solver(BarotropicSolver):
+    """Solver for two mode shallow water dynamics on a double periodic domain
+
+    uc = [u_0, v_0, u_1, u_2, v_1, v_2, t_1, t_2]
+    """
+    inds = [('u',0), ('v',0) ,('u',1), ('u',2), ('v', 1), ('v', 2), ('t',1), ('t', 2)]
+
+    def ix(self, uc):
+        return ixer(uc, self.inds)
+
+    def fx(self, fa, uc, dim='x'):
+
+        fa[:] = 0.0
+
+        q = self.ix(uc)
+        f = self.ix(fa)
+
+        if dim == 'x':
+            adv = 'u'
+        else:
+            adv = 'v'
 
 
+        # barotropic nonlinearity
+        for i in range(3):
+            f['u', 0] += q[adv, i] * q['u', i]
+            f['v', 0] += q[adv, i] * q['v', i]
+
+        # Advection by barotropic flow
+        for v, i in product(['u', 'v'], range(1,3)):
+            f[v, i] += q[adv, 0] * q[v, i]
+
+        for  i in range(1,3):
+            f['t', i] += q[adv, 0] * q['t', i] * i*i
+
+        f[adv, 1] -= q['t', 1]
+        f[adv, 2] -= q['t', 2]
+
+        # f['v', 1] -= 0
+        # f['v', 2] -= 0
+
+        f['t', 1] -= q[adv, 1]
+        f['t', 2] -= q[adv, 2]/4
+
+
+    def fy(self, fa, uc):
+        return self.fx(fa, uc, dim='y')
+
+    def ncvars(self, uc):
+        get_name = lambda name, m: "{}{}".format(name, m)
+        for i, namet in enumerate(self.inds):
+            yield get_name(*namet), ('x', 'y'), uc.validview[i]
+
+class SWE2NonlinearSolver(SWE2Solver):
+
+    ntrunc = 2
+    def __init__(self):
+        "docstring"
+        from math import sqrt, pi
+        from galerkin import flux_div_t, flux_div_u, sparsify
+
+        # get coefficients
+        au, bu = flux_div_u()
+        at, bt = flux_div_t()
+
+        self._au = sparsify(au)
+        self._bu = sparsify(bu)
+        self._at = sparsify(at)
+        self._bt = sparsify(bt)
+
+
+        # these setting replicate the barotropic only class
+        # self._au = [((0, 0,0), 1.0),
+        #             ((0, 1,1), 1.0),
+        #             ((0,2,2), 1.0),
+        #             ((1,0,1), 1.0),
+        #             ((2,0,2), 1.0)]
+
+        # self._at = [((1,0,1), 1.0),
+        #             ((2, 0, 2), 1.0)]
+        # self._bu = []
+        # self._bt = []
+    def flux_spec(self, dim):
+        """Specification of the flux  terms
+
+        These terms are given by
+        ((adv_vel)_j phi_k)_i
+        """
+
+        if dim == 'x':
+            adv = 'u'
+        elif dim == 'y':
+            adv = 'v'
+        else:
+            raise ValueError("Invalid dimension description")
+
+        out = []
+
+        # u advection terms
+        for (i,j,k),val in self._au:
+            for var in ['u', 'v']:
+                i1 = self.inds.index((var, i))
+                j1 = self.inds.index((adv, j))
+                k1 = self.inds.index((var, k))
+                out.append(((i1, j1, k1), val))
+
+        # temperature advection terms
+        for (i,j,k), val in self._at:
+            i1 = self.inds.index(('t', i))
+            j1 = self.inds.index((adv, j))
+            k1 = self.inds.index(('t', k))
+            out.append(((i1, j1, k1), val))
+
+        return out
+
+    def source_spec(self):
+        """Specification of the split-in-time source terms
+
+        These terms have are given by the form:
+            (- phi_k div u_j )_i
+        """
+
+
+        out = []
+
+        # u,v terms
+        for (i,j,k),val in self._bu:
+            for var in ['u', 'v']:
+                i1 = self.inds.index((var, i))
+                j1 = j
+                k1 = self.inds.index((var, k))
+                out.append(((i1, j1, k1), val))
+
+        # temperature terms
+        for (i,j,k), val in self._bt:
+            i1 = self.inds.index(('t', i))
+            j1 = j
+            k1 = self.inds.index(('t', k))
+            out.append(((i1, j1, k1), val))
+
+        return out
+
+    def fx(self, fa, uc, dim='x'):
+
+        fa[:] = 0
+
+        if dim == 'x':
+            adv = 'u'
+        else:
+            adv = 'v'
+
+        # provide access to variables by name
+        q = self.ix(uc)
+        f = self.ix(fa)
+
+        for (i, j, k), val in self.flux_spec(dim):
+            nemult(fa[i], val, uc[j] , uc[k])
+
+        for i in range(1,self.ntrunc+1):
+            f['t', i] -= q[adv, i]
+            f['t', i] /= i * i
+            f[adv, i] -= q['t', i]
+
+    def _extra_corrector(self, uc, dt):
+        super(SWE2NonlinearSolver, self)._extra_corrector(uc, dt)
+        # predictor corrector
+        f = self.split_terms(uc)
+        uc[:] += f*dt
+
+    def split_terms(self, uc):
+
+        dx, dy = self.geom.dx, self.geom.dy
+
+        fa = np.zeros_like(uc)
+
+        q = self.ix(uc)
+
+        # from scipy.ndimage import correlate1d
+        # w = {j:(correlate1d(q['u', j], [1/2/dx, 0, -1/2/dx], origin=0, axis=0)
+        #         + correlate1d(q['v', j], [1/2/dy, 0, -1/2/dy], origin=0, axis=1))
+        #      for j in range(1,self.ntrunc +1)}
+        w = {j:divergence(q['u', j], q['v',j], dx, dy)
+             for j in range(1,self.ntrunc +1)}
+
+        for (i,j,k), val in self.source_spec():
+             nemult(fa[i], val, w[j] , uc[k])
+
+        return fa
+
+    def advection_step(self, uc, dt):
+        self.central_scheme(uc, self.geom.dx, self.geom.dy, dt)
+
+        #predictor corrector
+        uc.exchange()
+        uc0 = uc.ghostview.copy()
+        f = self.split_terms(uc.ghostview)
+        uc.ghostview[:] += f*dt/2
 
 def main(plot=False):
 
     # Setup grid
     g = 4
-    nx, ny = 200, 200
-    Lx, Ly = pi, pi
+    nx, ny = 1000, 250
+    nx, ny = 512, 128
+    Lx, Ly = 26, 26 * (ny/nx)
 
     (x,y), (dx,dy) = ghosted_grid([nx, ny], [Lx, Ly], 0)
 
