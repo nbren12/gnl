e74f9b4b7fdf3f00db84c9dc217741c53aa61938
Noah D. Brenowitz <nbren12@gmail.com>
2017-10-02 00:17:03 -0700

    raise value error if stagger dim is not along blocking

--- packages/xnoah/xnoah/sam/coarsen.py
+++ packages/xnoah/xnoah/sam/coarsen.py
@@ -72,6 +72,9 @@ def coarsen_destagger_dask(x, blocks, stagger=None, mode='wrap'):
 def coarsen(A, blocks=None, stagger_dim=None, mode='wrap'):
     """coarsen and potentially destagger a 
     """
+    if stagger_dim not in blocks:
+        raise ValueError(f"stagger_dim \"{stagger_dim}\" is not in blocks")
+
     blocks = {k:blocks[k] for k in blocks
               if k in A.dims}
 
