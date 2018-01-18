5e10053bbfffee3021067a6d99b3e34ba82b3deb
Noah D. Brenowitz <nbren12@gmail.com>
2018-01-18 13:34:26 -0800

    add 2d coarse-graining to the desired output

--- workflows/sam/coarsegrain/run.sh
+++ workflows/sam/coarsegrain/run.sh
@@ -2,7 +2,8 @@
 
 file=/home/disk/eos13/guest/SAM6.10.6_NG/OUT_2D/NG_5120x2560x34_4km_10s_QOBS_EQX_4K_1280_0000861480.2Dcom_001.nc
 
-tgt=NG_5120x2560x34_4km_10s_QOBS_EQX_4K/coarse/2d/all.nc
+# tgt=NG_5120x2560x34_4km_10s_QOBS_EQX_4K/coarse/2d/all.nc
+tgt=NG_5120x2560x34_4km_10s_QOBS_EQX/coarse/2d/all.nc
 
 snakemake $tgt \
     --directory  /home/disk/eos4/nbren12/eos8/Data/id/2 \
