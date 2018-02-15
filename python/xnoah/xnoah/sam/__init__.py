f74a7f8096133e9abfd7bd6e4857bc7c65f671f4
Noah D. Brenowitz <nbren12@gmail.com>
2018-02-15 12:49:52 -0800

    add destagger to package level imports

--- python/xnoah/xnoah/sam/__init__.py
+++ python/xnoah/xnoah/sam/__init__.py
@@ -1,2 +1,2 @@
 from .regrid import (coarsen, get_dz, staggered_to_left, staggered_to_right,
-                     centered_to_left, centered_to_right)
+                     centered_to_left, centered_to_right, destagger)
