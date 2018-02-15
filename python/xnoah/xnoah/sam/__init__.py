823e7d0ee81191a18e37e5ff9615e96806c2761c
Noah D. Brenowitz <nbren12@gmail.com>
2018-02-15 12:59:08 -0800

    add coarsen_dim to package imports

--- python/xnoah/xnoah/sam/__init__.py
+++ python/xnoah/xnoah/sam/__init__.py
@@ -1,2 +1,3 @@
-from .regrid import (coarsen, get_dz, staggered_to_left, staggered_to_right,
-                     centered_to_left, centered_to_right, destagger)
+from .regrid import (coarsen, coarsen_dim, get_dz, staggered_to_left,
+                     staggered_to_right, centered_to_left, centered_to_right,
+                     destagger)
