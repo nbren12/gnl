a5a498cdaf21a833b74ca3b6492437ef3f59b69f
Noah D. Brenowitz <nbren12@gmail.com>
2018-05-07 11:16:09 -0700

    Add function for weighted eof analysis
    this function only works for lat/lon data at the moment.

--- python/xnoah/xnoah/__init__.py
+++ python/xnoah/xnoah/__init__.py
@@ -1,3 +1,4 @@
 from .xarray import swap_coord, copy_attrs
 from .xcalc import centderiv, cumtrapz
 from .xarray import integrate
+from .eofs import weighted_eofs
---
+++ python/xnoah/xnoah/eofs.py
@@ -0,0 +1,103 @@
+import numpy as np
+import xarray as xr
+from sklearn.decomposition import PCA
+import argparse
+
+
+def stack(x):
+    return x.stack(f=['lat', 'lon'])\
+        .transpose('time', 'f')
+
+
+def unstack(y, coords, dim_name='dim'):
+    dims = [dim_name, 'f']
+    coords = {
+        'f': coords['f'],
+        dim_name: np.arange(y.shape[0])
+    }
+    return xr.DataArray(y, dims=dims, coords=coords)\
+        .unstack('f')
+
+
+def weighted_eofs(da, wgt='coslat', n_components=10,
+                  component_dim_name='dim'):
+    """
+
+    Parameters
+    ----------
+    da : DataArray (lat, lon)
+        Input DataArray
+    wgt : DataArray (lat) or str
+        weights. Default: 'coslat'
+    n_components : int
+        number of EOFs to extract
+
+    Returns
+    -------
+    eofs_analysis: xr.Dataset
+        dataset with eofs, pcs, and singular values
+
+    """
+
+    if set(da.dims) < set(['lat', 'lon']):
+        raise NotImplementedError("This function only accepts lat/lon data at "
+                                  "the moment.")
+
+    if wgt == 'coslat':
+        wgt = np.cos(np.deg2rad(da.lat))
+
+    # flatten data
+    flat_data = stack(da * np.sqrt(wgt))
+
+    # fit pca
+    pca = PCA(n_components=n_components)
+    pca.fit(flat_data)
+
+    # get eofs
+    eofs = unstack(pca.components_, flat_data.coords,
+                   dim_name=component_dim_name) / np.sqrt(wgt)
+
+    # singular values
+    variance = xr.DataArray(pca.explained_variance_,
+                            dims=[component_dim_name],
+                            coords={component_dim_name: np.arange(n_components)})
+
+    # get pcs
+    dims = ['time', component_dim_name]
+    coords = {'time': flat_data.time,
+              component_dim_name: np.arange(n_components)}
+    pcs = pca.transform(flat_data)
+    pcs = xr.DataArray(pcs, dims=dims, coords=coords)
+
+    # prepare output dataset
+    return xr.Dataset({
+        'pcs': pcs,
+        'eofs': eofs,
+        'wgt': wgt,
+        'variance': variance
+    })
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input")
+    parser.add_argument("output")
+    parser.add_argument("-n", "--n_components", type=int, default=10)
+    parser.add_argument("-v", "--variable", type=str, default='olr')
+
+    args = parser.parse_args()
+
+    output = args.output
+    data_path = args.input
+    n_components = args.n_components
+
+    # prepare data
+    ds = xr.open_dataset(data_path)
+    olr = ds[args.variable].sel(time=slice('1980', None))
+
+    weighted_eofs(olr, n_components=n_components) \
+        .to_netcdf(output)
+
+
+if __name__ == '__main__':
+    main()
